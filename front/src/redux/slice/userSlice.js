3bc34cb5580a1aaacbe9d11bac19b6f2da0f125e
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2023-12-02 11:08:15 -0800

    adding login page

--- front/src/redux/slice/userSlice.js
+++ front/src/redux/slice/userSlice.js
@@ -3,11 +3,12 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
   name: "user",
   initialState: {
-    user: null,
+    user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null,
   },
   reducers: {
     login: (state, action) => {
       state.user = action.payload;
+      console.log(action.payload );
     },
     logout: (state) => {
       state.user = null;
